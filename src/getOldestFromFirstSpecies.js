29135948fd8523282cf45aecd5a4966f14189bb5
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-29 19:17:09 -0300

    adiciona função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
+const { employees, species } = data;
+
+function getOldestFromFirstSpecies(employeeId) {
   // seu código aqui
+  const animalId = employees.find(({ id }) => id === employeeId).responsibleFor[0];
+  const result = species.find(({ id }) => animalId === id)
+    .residents.reduce((acc, curr) => {
+      if (acc.age > curr.age) return acc;
+      return curr;
+    });
+  return Object.values(result);
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
