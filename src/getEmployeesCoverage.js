36b531193ff2870570711aab39267b837c393d80
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-29 20:13:30 -0300

    getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const { employees, species } = data;
+
+const testInfo = ((info) => {
+  const [keyInfo] = Object.keys(info);
+  const verifica = employees.some(({ id, firstName, lastName }) =>
+    id === info[keyInfo] || firstName === info[keyInfo] || lastName === info[keyInfo]);
+  if (!verifica) throw new Error('Informações inválidas');
+});
+
+const infoValida = ((dataEmployee) => ({
+  id: dataEmployee.id,
+  fullName: `${dataEmployee.firstName} ${dataEmployee.lastName}`,
+  species: dataEmployee.responsibleFor.map((animalId) =>
+    species.find(({ id }) => id === animalId).name),
+  locations: dataEmployee.responsibleFor.map((animalId) =>
+    species.find(({ id }) => id === animalId).location),
+}));
+
+function getEmployeesCoverage(info) {
   // seu código aqui
+  if (info) {
+    testInfo(info);
+    const [keyInfo] = Object.keys(info);
+    const getEmployee = employees.find(({ id, firstName, lastName }) =>
+      id === info[keyInfo] || firstName === info[keyInfo] || lastName === info[keyInfo]);
+    return infoValida(getEmployee);
+  }
+  return employees.map((item) => infoValida(item));
 }
 
 module.exports = getEmployeesCoverage;
+
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+// console.log(getEmployeesCoverage());
