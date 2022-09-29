7eb5a7266489007624f5def7b09533e817a5f71d
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-29 15:12:24 -0300

    adiciona a função getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  return employees.find(({ firstName, lastName }) =>
+    firstName === employeeName || lastName === employeeName);
 }
+
+// console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
