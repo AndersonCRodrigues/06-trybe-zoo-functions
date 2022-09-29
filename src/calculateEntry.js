72c08afe2643fb3ce972f19eb19a71c7a2289c89
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-29 17:16:39 -0300

    adiciona função calculateEntry e countEntrants

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
+const { prices } = data;
+
+function countEntrants(entrants = []) {
   // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach(({ age }) => {
+    if (age < 18) child += 1;
+    if (age >= 18 && age < 50) adult += 1;
+    if (age >= 50) senior += 1;
+  });
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
   // seu código  aqui
+  if (!entrants || Object.entries(entrants).length < 1) return 0;
+  const visitas = countEntrants(entrants);
+  const arrVisitas = Object.keys(visitas);
+  return arrVisitas.reduce((acc, curr) => acc + (visitas[curr] * prices[curr]), 0);
 }
+
+console.log(calculateEntry({}));
 
 module.exports = { calculateEntry, countEntrants };
