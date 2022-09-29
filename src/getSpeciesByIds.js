369ef846c8ed6315fd154b34ccb5db072dc7beab
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-29 14:56:15 -0300

    adiciona função getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (!ids) return [];
+  return species.filter(({ id }) => ids.includes(id));
 }
 
 module.exports = getSpeciesByIds;
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
