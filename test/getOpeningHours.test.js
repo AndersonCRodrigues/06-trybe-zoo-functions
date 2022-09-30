1869ce1cbaa06cef1e8a0c4fe039f010f9d5a0f1
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-30 14:01:26 -0300

    implementa testes

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,35 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  test('testa se getOpeningHours é uma função', () => {
+    expect(typeof getOpeningHours).toBe('function');
+  });
+  test('testa os horários de acodo com o dia da semana', () => {
+    expect(getOpeningHours('Monday', '12:00-AM')).toBe('The zoo is closed');
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+    expect(getOpeningHours('Wednesday', '00:00-AM')).toBe('The zoo is closed');
+  });
+  test('testa se retorna um erro throw ao passar paramentro inválido', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow('The day must be valid. Example: Monday');
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow('The hour should represent a number');
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow('The minutes should represent a number');
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+    expect(() => getOpeningHours('Monday', '13:00-AM')).toThrow('The hour must be between 0 and 12');
+    expect(() => getOpeningHours('Tuesday', '09:60-AM')).toThrow('The minutes must be between 0 and 59');
+  });
+  test('testa o retorno sem parametro', () => {
+    const result = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(getOpeningHours()).toEqual(result);
+  });
+  test('', () => {
+
+  });
+});
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,24 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Testa se existe a função getElephants', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+  test('Testa o retorno sem parametros', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  test('Testa o retorno para passagem de parametro que não seja string', () => {
+    expect(handlerElephants(22)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  test('Testa o retorno para string vazia', () => {
+    expect(handlerElephants('')).toBe(null);
+  });
+  test('Testo o retorno para parametros corretos', () => {
+    expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(handlerElephants('averageAge')).toBe(10.5);
+    expect(handlerElephants('location')).toBe('NW');
+    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});
